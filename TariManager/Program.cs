1f252aa9eff84286153a6ccdf7b236b82e890585
dubomire <dubrovin.g@skbkontur.ru>
2021-09-29 18:02:56 +0300

    Initial solution created

---
+++ TariManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TariManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
